3ab5cf7aa11087141e47e91c4be344cf352b9de5
Miles B Huff <MilesBHuff@users.noreply.github.com>
2017-08-15 01:22:52 -0400

    Update role.ranger.js

--- src/role.ranger.js
+++ src/role.ranger.js
@@ -1,6 +1,9 @@
 var roleRanger = {
-	run: function (creep) {
-		//TODO
+    run: function (creep) {
+		var hostile = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+		if(hostile) {
+			creep.attack(hostile);
+		}
 	}
 };
 module.exports = roleRanger;
