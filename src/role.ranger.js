61aa068065ea019381e16c4d692167cb15f26cb7
Miles B Huff <MilesBHuff@users.noreply.github.com>
2017-08-19 00:36:49 -0400

    Delete role.ranger.js

--- src/role.ranger.js
+++
@@ -1,9 +0,0 @@
-var roleRanger = {
-    run: function (creep) {
-		var hostile = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
-		if(hostile) {
-			creep.attack(hostile);
-		}
-	}
-};
-module.exports = roleRanger;
