152113fd08a6d76daa9e7eaf6ebc496d695c5c81
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-08-19 03:13:44 -0400

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -1,15 +1,14 @@
 var actions = {
 	wander: function (creep) {
-			var x = Math.round(Math.random());
-			var y = Math.round(Math.random());
-			if(Math.round(Math.random())) {
-				x *= -1;
-			}
-			if(Math.round(Math.random())) {
-				y *= -1;
-			}
-			creep.moveTo(creep.pos.x + x, creep.pos.y + y);
+		var x = Math.round(Math.random());
+		var y = Math.round(Math.random());
+		if(Math.round(Math.random())) {
+			x *= -1;
 		}
+		if(Math.round(Math.random())) {
+			y *= -1;
+		}
+		creep.moveTo(creep.pos.x + x, creep.pos.y + y);
 	}
 };
 module.exports = actions;
