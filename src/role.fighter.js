22ef957b1d6086a31b789ecf548725df9e136dd8
Miles B Huff <MilesBHuff@users.noreply.github.com>
2017-08-19 04:29:05 -0400

    Update role.fighter.js

--- src/role.fighter.js
+++ src/role.fighter.js
@@ -1,15 +1,18 @@
 var roleFighter = {
     run: function (creep) {
-		
+
 		// Attack any enemy units present in the room
 		// ---------------------------------------------------------------------
-		var target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
-		if(target) {
-			creep.attack(target);
+		creep.memory.target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS).id;
+		if(creep.attack(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE) {
+			if(creep.moveTo(Game.getObjectById(creep.memory.target), {visualizePathStyle: {stroke: "#f00"}}) == ERR_NO_PATH) {
+				creep.memory.target = undefined;
+			}
+		}
 
 		// If there is no imminent threat, wander around, so as not to interfere with the workers.
 		// ---------------------------------------------------------------------
-		} else {
+		else {
 			require("actions").wander(creep);
 		}
 	}
