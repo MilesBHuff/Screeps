24509e6c896f1cb188c45d263bd47ea0ca711e5d
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-08-19 03:14:23 -0400

    Update role.fighter.js

--- src/role.fighter.js
+++ src/role.fighter.js
@@ -10,15 +10,7 @@ var roleFighter = {
 		// If there is no imminent threat, wander around, so as not to interfere with the workers.
 		// ---------------------------------------------------------------------
 		} else {
-			var x = Math.round(Math.random());
-			var y = Math.round(Math.random());
-			if(Math.round(Math.random())) {
-				x *= -1;
-			}
-			if(Math.round(Math.random())) {
-				y *= -1;
-			}
-			creep.moveTo(creep.pos.x + x, creep.pos.y + y);
+			require("actions").wander(creep);
 		}
 	}
 };
