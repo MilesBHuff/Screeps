f3186c319a91f7ac5ff383ffa580d7db91bcb24b
Miles B Huff <MilesBHuff@users.noreply.github.com>
2017-08-19 02:53:55 -0400

    Update role.fighter.js

--- src/role.fighter.js
+++ src/role.fighter.js
@@ -3,6 +3,15 @@ var roleFighter = {
 		var hostile = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
 		if(hostile) {
 			creep.attack(hostile);
+		} else {
+			var x = Math.ceil(Math.random() * 6);
+			var y = Math.ceil(Math.random() * 6);
+			if(Math.floor(Math.random()) {
+				if(Math.floor(Math.random()) {
+					   x*= -1;
+				} else y*= -1;
+			}
+			creep.moveTo(x, y);
 		}
 	}
 };
