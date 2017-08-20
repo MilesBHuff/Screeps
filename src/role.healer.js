b6767e41db9872244988c2d86191ada7d5b308be
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-08-20 13:20:44 -0400

    Update role.healer.js

--- src/role.healer.js
+++ src/role.healer.js
@@ -1,6 +1,23 @@
 var roleHealer = {
 	run: function (creep) {
-		require("actions").wander(creep);
+		
+		// Heal any damaged allied units present in the room
+		// ---------------------------------------------------------------------
+		var target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {filter: (eachCreep) => eachCreep.hits < eachCreep.hitsMax});
+	    	if(target && target.id) {
+			creep.memory.target = target.id;
+			if(creep.heal(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE) {
+				if(creep.moveTo(Game.getObjectById(creep.memory.target), {visualizePathStyle: {stroke: "#f00"}}) == ERR_NO_PATH) {
+					creep.memory.target = undefined;
+				}
+			}
+		}
+
+		// If there is no imminent threat, wander around, so as not to interfere with the workers.
+		// ---------------------------------------------------------------------
+		else {
+			require("actions").wander(creep);
+		}
 	}
 };
 module.exports = roleHealer;
