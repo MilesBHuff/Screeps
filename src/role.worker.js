ca4005525470852d7f41f93388eb6cf6c1424ea3
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-08-20 11:55:32 -0400

    Update role.worker.js

--- src/role.worker.js
+++ src/role.worker.js
@@ -69,10 +69,11 @@ var roleWorker = {
 					var targetType = 0;
 					// Make sure we always have the max number of creeps
 					if(i == 0
-					&& creep.room.memory.claimerLimit >= _.filter(Game.creeps, (eachCreep) => eachCreep.memory.role == DEFINES.ROLES.CLAIMER && eachCreep.room == creep.room).length
-					&& creep.room.memory.fighterLimit >= _.filter(Game.creeps, (eachCreep) => eachCreep.memory.role == DEFINES.ROLES.FIGHTER && eachCreep.room == creep.room).length
-					&& creep.room.memory.healerLimit  >= _.filter(Game.creeps, (eachCreep) => eachCreep.memory.role == DEFINES.ROLES.HEALER  && eachCreep.room == creep.room).length
-					&& creep.room.memory.workerLimit  >= _.filter(Game.creeps, (eachCreep) => eachCreep.memory.role == DEFINES.ROLES.WORKER  && eachCreep.room == creep.room).length) {
+					&& creep.room.memory.claimerLimit <= _.filter(Game.creeps, (eachCreep) => eachCreep.memory.role == DEFINES.ROLES.CLAIMER && eachCreep.room == creep.room).length
+					&& creep.room.memory.fighterLimit <= _.filter(Game.creeps, (eachCreep) => eachCreep.memory.role == DEFINES.ROLES.FIGHTER && eachCreep.room == creep.room).length
+					&& creep.room.memory.healerLimit  <= _.filter(Game.creeps, (eachCreep) => eachCreep.memory.role == DEFINES.ROLES.HEALER  && eachCreep.room == creep.room).length
+					&& creep.room.memory.workerLimit  <= _.filter(Game.creeps, (eachCreep) => eachCreep.memory.role == DEFINES.ROLES.WORKER  && eachCreep.room == creep.room).length
+					){
 						targetType = Math.floor(Math.random() * 4);
 					}
 					switch(targetType) {
