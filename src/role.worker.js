9aeec3b40f088bc72ae1d65ca4c6e5fc915f4985
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-08-21 04:52:52 -0400

    Update role.worker.js

--- src/role.worker.js
+++ src/role.worker.js
@@ -117,7 +117,7 @@ var roleWorker = {
 					// If there's no new energy available, use what you're already carrying, if anything.
 					if(creep.carry.energy > 0) {
 						creep.memory.harvesting = false;
-					}
+					} else break;
 				}
 
 				// Always keep spawns and extensions filled up to max.
