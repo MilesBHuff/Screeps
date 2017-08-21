c87d8be6d44a261e11ec42042d336f53ef8990c8
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-08-21 01:52:08 -0400

    Create commands.js

---
+++ src/commands.js
@@ -0,0 +1,4 @@
+if(false) {
+	// Set a sign on the specified controller
+	var sign = Game.getObjectById().sign; sign.time = Game.time; sign.datetime = Date(); sign.username = "MilesBHuff"; sign.text = "";
+}
