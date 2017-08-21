402c18effb6406f0460c5d6d9479e0e21295acf2
Miles B Huff <MilesBHuff@users.noreply.github.com>
2017-08-21 02:19:31 -0400

    Update commands.js

--- src/commands.js
+++ src/commands.js
@@ -1,4 +1,7 @@
 if(false) {
-	// Set a sign on the specified controller
-	Game.getObjectById("").sign = {time: Game.time, datetime: Date(), username: "MilesBHuff", text: ""};
+	// Set a sign on the specified controller with the specified creep
+	Game.getObjectById("Creep ID").signController(Game.getObjectById("Controller ID"), "https://GitHub.com/MilesBHuff/Screeps");
+	// Move the specified creep towards coordinates
+	Game.getObjectById("Creep ID").moveTo(x, y);
+	// Spawn a creep at the specified spawn
 }
