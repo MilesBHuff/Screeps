e7f858d1d2c8d528b266bfe70157c2a56f121361
Miles B Huff <MilesBHuff@users.noreply.github.com>
2017-08-21 03:14:29 -0400

    Update role.tower.js

--- src/role.tower.js
+++ src/role.tower.js
@@ -1,6 +1,51 @@
+// role.tower.js
+// #############################################################################
+
+// Non-member variables
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+const DEFINES = require("defines");
+
+// Define the role
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 var roleTower = {
 	run: function (structure) {
-		//TODO
+
+		// Variables
+		// =================================================================
+		var repairLimit = DEFINES.REPAIR_LIMIT * creep.room.controller.level;
+		for(var b = true; b; b = false) {
+			
+			// Heal the closest damaged allied unit
+			// =================================================================
+			var target = structure.pos.findClosestByRange(FIND_MY_CREEPS, {filter: (creep) =>
+				   creep.hits < creep.hitsMax
+			}););
+			if(target && target.id) {
+				structure.heal(Game.getObjectById(target))
+				break;
+			}
+			
+			// Attack the closest enemy unit
+			// =================================================================
+			var target = structure.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+			if(target && target.id) {
+				structure.attack(Game.getObjectById(target))
+				break;
+			}
+			
+			// Repair the closest damaged structure
+			// ---------------------------------------------------------------------
+			// Only repair structures that are at least 25% of the way damaged, either from their repair maximum, or the global repair maximum.
+			// It would seem that walls cannot be owned, so we have to search through all targets in the room, not just our own.
+			var target = structure.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structureEach) =>
+				   structureEach.hits < (structureEach.hitsMax * 0.75)
+				&& structureEach.hits < (repairLimit * 0.75)
+			});
+			if(target && target.id) {
+				structure.repair(Game.getObjectById(target))
+				break;
+			}
+		}
 	}
 };
 module.exports = roleTower;
