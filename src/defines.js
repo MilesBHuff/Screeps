a5b084ad3041f1a7cb8004c605a22084ddceeb27
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-08-22 00:34:36 -0400

    Update defines.js

--- src/defines.js
+++ src/defines.js
@@ -1,16 +1,39 @@
 // defines.js
 // #############################################################################
-/** This file contains constants and variables that are used across multiple
- *  files.
+/** This file contains constant variables and functions that are used across
+ *  multiple files.
 **/
 
-// Constants and Variables
+// Constant variables and functions
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-const defines = {
+const DEFINES = {
 	
+	// Functions
+	// =========================================================================
+	
+	// Wander
+	// -------------------------------------------------------------------------
+	/** Makes the given creep wander around its room at random.
+	 * @param creep The creep to control.
+	**/
+	WANDER: function (creep) {
+		if(creep.pos.x <  3
+		|| creep.pos.x > 46
+		|| creep.pos.y <  3
+		|| creep.pos.y > 46
+		){ creep.moveTo(24, 24);
+		} else {
+			var direction = Math.round(Math.random() * 8);
+			if(direction) {
+				creep.move(direction);
+			}
+		}
+	}
+	
+	// Variables
+	// =========================================================================
 	// This is the base limit to which things should be repaired.  It should be multiplied by the room in-question's current control level.
 	REPAIR_LIMIT: 24000,
-
 	// These are all the roles available for creeps
 	ROLES: Object.freeze({
 		"WORKER":  0,
@@ -18,4 +41,4 @@ const defines = {
 		"HEALER":  2,
 	}),
 };
-module.exports = defines;
+module.exports = DEFINES;
