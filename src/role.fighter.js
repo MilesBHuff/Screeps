788ff15fc0f611d2244372f59f381574ad92096a
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-08-22 03:50:49 -0400

    Update role.fighter.js

--- src/role.fighter.js
+++ src/role.fighter.js
@@ -29,6 +29,20 @@ var roleFighter = {
 			}
 		}
 
+		// Attack any enemy structures present in the room
+		// ====================================================================
+		var target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES);
+	    	if(target && target.id) {
+			creep.memory.target = target.id;
+			if(creep.rangedAttack(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE
+			|| creep.attack(      Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE
+			){
+				if(creep.moveTo(Game.getObjectById(creep.memory.target), {visualizePathStyle: {stroke: "#f00"}}) == ERR_NO_PATH) {
+					creep.memory.target = undefined;
+				}
+			}
+		}
+
 		// If there is no imminent threat, wander around, so as not to interfere with the workers.
 		// ====================================================================
 		else {
