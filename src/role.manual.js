bf373e29b7d50f2a29429cb0cc5d8f1b78385ce1
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-08-22 01:25:28 -0400

    Update role.manual.js

--- src/role.manual.js
+++ src/role.manual.js
@@ -1,8 +1,28 @@
+// role.fighter.js
+// #############################################################################
+/** This script is designed to streamline manual control of creeps.
+**/
+
+// Variables
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+const DEFINES = require("defines");
 var roleManual = {
+	
+	// run()
+	// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	/** This function controls the provided creep.
+	 * @param creep The creep to control.
+	**/
 	run: function (creep) {
+		
+		// Move to the provided destination, if there is one.
+		// ====================================================================
 		if(creep.memory._move.dest) {
     		creep.moveTo(creep.memory._move.dest.x, creep.memory._move.dest.y);
 		}
 	}
 };
+
+// Export this file for use in others.
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 module.exports = roleManual;
