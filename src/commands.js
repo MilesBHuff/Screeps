324827dfab35a1f81b4c8c0f43758d5a504e266a
Miles B Huff <MilesBHuff@users.noreply.github.com>
2017-08-22 01:28:10 -0400

    Update commands.js

--- src/commands.js
+++ src/commands.js
@@ -1,3 +1,8 @@
+// commands.js
+// #############################################################################
+/** This file lists various prewritten commands for manual use at the console.
+**/
+
 if(false) {
 	// Set a sign on the specified controller with the specified creep
 	Game.getObjectById("Creep ID").signController(Game.getObjectById("Controller ID"), "GitHub.com/MilesBHuff/Screeps");
