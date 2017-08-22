8506ed7a6f6f30d94e401f5d7f5bf1f06e90aa33
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-08-22 03:54:49 -0400

    Update role.fighter.js

--- src/role.fighter.js
+++ src/role.fighter.js
@@ -14,38 +14,40 @@ var roleFighter = {
 	 * @param creep The creep to control.
 	**/
 	run: function (creep) {
+		for(var b = true; b; b = false) {
 
-		// Attack any enemy units present in the room
-		// ====================================================================
-		var target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
-	    	if(target && target.id) {
-			creep.memory.target = target.id;
-			if(creep.rangedAttack(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE
-			|| creep.attack(      Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE
-			){
-				if(creep.moveTo(Game.getObjectById(creep.memory.target), {visualizePathStyle: {stroke: "#f00"}}) == ERR_NO_PATH) {
-					creep.memory.target = undefined;
+			// Attack any enemy units present in the room
+			// ====================================================================
+			var target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+			if(target && target.id) {
+				creep.memory.target = target.id;
+				if(creep.rangedAttack(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE
+				|| creep.attack(      Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE
+				){
+					if(creep.moveTo(Game.getObjectById(creep.memory.target), {visualizePathStyle: {stroke: "#f00"}}) == ERR_NO_PATH) {
+						creep.memory.target = undefined;
+					}
 				}
+				break;
 			}
-		}
 
-		// Attack any enemy structures present in the room
-		// ====================================================================
-		var target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES);
-	    	if(target && target.id) {
-			creep.memory.target = target.id;
-			if(creep.rangedAttack(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE
-			|| creep.attack(      Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE
-			){
-				if(creep.moveTo(Game.getObjectById(creep.memory.target), {visualizePathStyle: {stroke: "#f00"}}) == ERR_NO_PATH) {
-					creep.memory.target = undefined;
+			// Attack any enemy structures present in the room
+			// ====================================================================
+			var target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES);
+			if(target && target.id) {
+				creep.memory.target = target.id;
+				if(creep.rangedAttack(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE
+				|| creep.attack(      Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE
+				){
+					if(creep.moveTo(Game.getObjectById(creep.memory.target), {visualizePathStyle: {stroke: "#f00"}}) == ERR_NO_PATH) {
+						creep.memory.target = undefined;
+					}
 				}
+				break;
 			}
-		}
 
-		// If there is no imminent threat, wander around, so as not to interfere with the workers.
-		// ====================================================================
-		else {
+			// If there is no imminent threat, wander around, so as not to interfere with the workers.
+			// ====================================================================
 			DEFINES.WANDER(creep);
 		}
 	}
