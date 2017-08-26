e41cd1af180ecc01cab1f0feccabd9fb0033d91c
Miles B Huff <MilesBHuff@users.noreply.github.com>
2017-08-26 02:31:34 -0400

    Update role.fighter.js

--- src/role.fighter.js
+++ src/role.fighter.js
@@ -29,9 +29,20 @@ var roleFighter = {
 		if(!creep.memory || !creep.memory.target) {
 			var targets = Array();
 			for(var b = true; b; b = false) {
+				// Attack enemy creeps
 				targets = creep.room.find(FIND_HOSTILE_CREEPS);
 				if(targets.length) break;
+				// Attack enemy structures
 				targets = creep.room.find(FIND_HOSTILE_STRUCTURES);
+				if(targets.length) break;
+				// Attack condemned structures
+				if(creep.room.memory && creep.room.memory.dismantle) {
+					targets = Array();
+					for(var a = 0; room.memory.dismantle[a]; a++) {
+						targets.push(Game.getObjectById(creep.room.memory.dismantle[a]));
+					};
+					if(targets && targets.length) break;
+				}
 				if(targets.length) break;
 			}
 			if(targets.length) {
