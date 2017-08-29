8ddebea618ee9bffd7fbea4745df599656546b20
Miles B Huff <MilesBHuff@users.noreply.github.com>
2017-08-29 02:19:09 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -59,7 +59,7 @@ module.exports.loop = function () {
 			var exitsCount = 0;
 			for(var i = 0; i < 4; i++) {
 				var index = ((2 * i) + 1).toString();
-				if(!exits[index] && !(Game.rooms[exits[index].name] && Game.rooms[exits[index].name].my)) {
+				if(!exits[index] && !(Game.rooms[exits[index]] && Game.rooms[exits[index]].my)) {
 					exitsCount++;
 				} //fi
 			} //done
