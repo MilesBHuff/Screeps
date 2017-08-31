f765cc50f8df68e38facf9c4dde7282616395c6e
Miles B Huff <MilesBHuff@users.noreply.github.com>
2017-08-31 00:59:53 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -52,8 +52,8 @@ module.exports.loop = function () {
 			// -----------------------------------------------------------------
 			// Then, modify the number of workers per the level of the controller.
 			room.memory.workerLimit += (Math.round((CONTROLLER_STRUCTURES[STRUCTURE_EXTENSION][DEFINES.CONTROLLER_LEVEL_MAX] - room.find(FIND_MY_STRUCTURES, {filter: (structure) => {return(structure.structureType == STRUCTURE_EXTENSION);}}).length) / 10)) / 2;
-			// Multiply the number of workers by the number of sources in the room.
-			room.memory.workerLimit *= room.find(FIND_SOURCES).length + room.find(FIND_MY_STRUCTURES, {filter: (structure) => {return(structure.structureType == STRUCTURE_EXTRACTOR);}}).length;
+			// Multiply the number of workers by the number of sources and mineral extractors in the room.
+			room.memory.workerLimit *= room.find(FIND_SOURCES).length; // + room.find(FIND_MY_STRUCTURES, {filter: (structure) => {return(structure.structureType == STRUCTURE_EXTRACTOR);}}).length;
 
 			// Set up the fighter creep limit
 			// -----------------------------------------------------------------
