b32c9b97446a889635e34560b72ddd81d1691484
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2018-07-15 06:18:29 -0400

    Fixed and consolidated spawning

--- src/role.room.js
+++ src/role.room.js
@@ -69,8 +69,10 @@ let roleRoom = {
             function setWorkerLimit() {
                 // Modify the number of workers per the level of the controller.
                 room.memory.workerLimit += (Math.round((CONTROLLER_STRUCTURES[STRUCTURE_EXTENSION][LIB_COMMON.CONTROLLER_LEVEL_MAX] - room.find(FIND_MY_STRUCTURES, {filter: (structure) => {return(structure.structureType === STRUCTURE_EXTENSION);}}).length) / 10)) / 2;
+				if(room.memory.workerLimit <= 0) room.memory.workerLimit = 1;
                 // Multiply the number of workers by the number of sources and mineral extractors in the room.
                 room.memory.workerLimit *= room.find(FIND_SOURCES).length; // + room.find(FIND_MY_STRUCTURES, {filter: (structure) => {return(structure.structureType === STRUCTURE_EXTRACTOR);}}).length;
+				if(room.memory.workerLimit <= 0) room.memory.workerLimit = 1;
             } //setWorkerLimit
 
             // Fighters
