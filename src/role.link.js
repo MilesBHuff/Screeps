49abf5da6f9b2a5da04730a285a1ac139509f8c2
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2018-10-28 16:36:58 -0400

    Fleshed out the link controller

--- src/role.link.js
+++ src/role.link.js
@@ -12,7 +12,30 @@ let roleLink = {
     /** This function controls the provided link.
      * @param link The link to control
     **/
-    run: function (link) {
+    run: function(link) {
+		// Preliminary verifications
+		if(!link || link.structureType !== STRUCTURE_LINK) return ERR_INVALID_TARGET;
+		if(link.cooldown > 0) return ERR_TIRED;
+
+		// Find other links
+		let targets = link.room.find(FIND_MY_STRUCTURES, {
+			filter: (newLink) => {return(
+				   newLink.room.memory.dismantle.indexOf(newLink.id) < 0
+				&& newLink.structureType === STRUCTURE_LINK
+				&&(link.energy && newLink.energy
+				&& link.energy >  newLink.energy
+				)
+			);}
+		});
+		if(targets.length < 1) return OK;
+
+		// Sort them by energy
+		//TODO
+
+		// Equalize with the lowest-energy link
+		let transferAmount = link.energy - ((link.energy + targets[0].energy) / 2);
+		link.transferEnergy(targets[0], transferAmount);
+		return OK;
     }, //run
 }; //roleLink
 
