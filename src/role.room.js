da47b57330892f350cce5fcabed91fcfa9820bfe
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2018-10-29 04:26:51 -0400

    Fixed fighter spawns

--- src/role.room.js
+++ src/role.room.js
@@ -87,8 +87,8 @@ let roleRoom   = {
                 // Count the number of exits to uncontrolled rooms
                 let exits      = Game.map.describeExits(room.name);
                 let exitsCount = 0;
-                if(exits) {
-                    for(let i = 0; i < exits.length; i++) {
+                if(exits && exits.length && exits.length === 4) {
+                    for(let i = 0; i < 4; i++) {
                         let index = ((2 * i) + 1).toString();
                         if(exits[index]
                             &&  !( Game.rooms[exits[index]]
