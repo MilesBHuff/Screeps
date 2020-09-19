695173090507a48d086d3576770663e71c1f5dd1
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-09-19 12:13:32 -0400

    Started refactoring Commands

--- screeps/src/Commands.ts
+++ screeps/src/Commands.ts
@@ -1,19 +1,19 @@
-// lib.commands.js
-// #############################################################################
+////////////////////////////////////////////////////////////////////////////////
 /** This file lists letious prewritten commands for manual use at the console.
  *  Make sure to include it before trying to use its functions!
 **/
-"use strict";
+module.exports.commands = class Commands {
 
-// Variables
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-const LIB_MISC = require("lib.misc");
-const LIB_MOVE = require("lib.move");
-const ROLE_SPAWN = require("role.spawn");
-const LIB_COMMANDS = {
+    ////////////////////////////////////////////////////////////////////////////////
+    constructor(
+        private readonly miscLib = require(`Libs/MiscLib`),
+        private readonly moveLib = require(`Libs/MoveLib`),
+        private readonly spawnAI = require(`AIs/Structures/SpawnAI`),
+    ) {
+        return this;
+    }
 
-    // Check progress
-    // =========================================================================
+    ////////////////////////////////////////////////////////////////////////////////
     // require("lib.commands").checkProgress("StructureID");
     /** Check the exact progress of a controller or construction site.  If the
      *  specified structure does not have a progress letiable, use its repair
@@ -21,7 +21,7 @@ const LIB_COMMANDS = {
      * @param  structureId
      * @return an exit-code.
     **/
-    checkProgress: function(structureId) {
+    public checkProgress(structureId) {
         let message   = "";
         let structure = Game.getObjectById(structureId);
         /*//*/ if(structure.progress && structure.progressTotal) {
@@ -31,10 +31,9 @@ const LIB_COMMANDS = {
         } else return ERR_INVALID_TARGET;
         LIB_MISC.say(structure, message);
         return message;
-    }, //function
+    }
 
-    // Create creep
-    // =========================================================================
+    ////////////////////////////////////////////////////////////////////////////////
     // require("lib.commands").createCreep("SpawnName", "MANUAL");
     /** This function spawns a creep at the desired spawn, using the same
      *  creep-generation function as main().
@@ -43,60 +42,52 @@ const LIB_COMMANDS = {
      * @param  dryRun    Whether to do a dry run.
      * @return an exit-code.
     **/
-    createCreep: function(spawnName, roleName, dryRun) {
+   public createCreep(spawnName, roleName, dryRun) {
         return ROLE_SPAWN.spawnCreep(Game.spawns[spawnName], LIB_MISC.ROLES[roleName], dryRun ? true : false);
-    }, //function;
+    }
 
-    // Dismantle
-    // =========================================================================
+    ////////////////////////////////////////////////////////////////////////////////
     // require("lib.commands").dismantle("StructureID");
     /** Marks the specified structure for demolition.
      * @param  structureId The ID of the structure to dismantle.
      * @return An exit-code.
     **/
-    dismantle: function(structureId) {
+   public dismantle(structureId) {
         return Game.getObjectById(structureId).room.memory.dismantle.push(structureId);
-    }, //function
+    }
 
-    // Move creep to
-    // =========================================================================
+    ////////////////////////////////////////////////////////////////////////////////
     // require("lib.commands").moveCreepTo("CreepName", "TargetID");
     /** Order a creep to move to a particular target.
      * @param  creepName The name of the creep to use
      * @param  target    The ID of the target to move towards
      * @return an exit-code.
     **/
-    moveCreepTo: function(creepName, target) {
+    public moveCreepTo(creepName, target) {
         Game.creeps[creepName].memory.target = target;
         return LIB_MOVE.move(Game.creeps[creepName], COLOR_GREY, false);
-    }, //function
+    }
 
-    // Remove construction
-    // =========================================================================
+    ////////////////////////////////////////////////////////////////////////////////
     // require("lib.commands").removeConstruction();
     /** Remove all construction sites
      * @return an exit-code.
     **/
-    removeConstruction: function() {
+    public removeConstruction() {
         for(let name in Game.constructionSites) {
             Game.constructionSites[name].remove();
         }
         return OK;
-    }, //function
+    }
 
-    // Sign controller
-    // =========================================================================
+    ////////////////////////////////////////////////////////////////////////////////
     // require("lib.commands").signController("CreepName", "ControllerID", "Message");
     /** Signs the specified controller with the specified creep.
      * @param  creepName    The name of the creep to use
      * @param  controllerId The ID of the controller to sign
      * @param  message      The message to sign the controller with
     **/
-    signController: function(creepName, controllerId, message) {
+   public signController(creepName, controllerId, message) {
         return Game.creeps[creepName].signController(Game.getObjectById(controllerId), message);
-    }, //function
-}; //struct
-
-// Export this file for use in others.
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-module.exports = LIB_COMMANDS;
+    }
+};
